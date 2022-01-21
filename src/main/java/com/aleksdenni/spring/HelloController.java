efc12953f1c74785ae5975cd27eff4635b94da92
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2022-01-21 15:23:04 +0200

    study controller

--- src/main/java/com/aleksdenni/spring/HelloController.java
+++ src/main/java/com/aleksdenni/spring/HelloController.java
@@ -2,11 +2,14 @@ package com.aleksdenni.spring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("hello-world")
+    //@GetMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String sayHello(){
         return "hello_world";
     }
--- src/main/java/com/aleksdenni/spring/config/SpringConfig.java
+++ src/main/java/com/aleksdenni/spring/config/SpringConfig.java
@@ -5,20 +5,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.thymeleaf.Thymeleaf;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafView;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @ComponentScan("com.aleksdenni.spring")
 @EnableWebMvc
-public class SpringConfig {
+public class SpringConfig implements WebMvcConfigurer {
+
     private final ApplicationContext applicationContext;
 
     @Autowired
@@ -27,7 +25,7 @@ public class SpringConfig {
     }
 
     @Bean
-    public SpringResourceTemplateResolver templateResolver(){
+    public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/views/");
@@ -36,15 +34,15 @@ public class SpringConfig {
     }
 
     @Bean
-    public SpringTemplateEngine templateEngine(){
+    public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
     }
 
-    @Bean
-    public void configureViewResolvers(ViewResolverRegistry registry){
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
