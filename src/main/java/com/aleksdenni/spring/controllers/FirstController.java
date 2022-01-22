713dec67e666cfc48ecb368b6b3bdb082b51a43f
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2022-01-22 10:12:03 +0200

    maked calculator

--- src/main/java/com/aleksdenni/spring/controllers/FirstController.java
+++ src/main/java/com/aleksdenni/spring/controllers/FirstController.java
@@ -1,8 +1,10 @@
 package com.aleksdenni.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,15 +13,46 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request){
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-        System.out.println("Hello, " + name + " " + surname);
+    public String helloPage(@RequestParam(value="name", required = false)String name,
+                            @RequestParam(value="surname", required = false)String surname,
+                            Model model){
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage(){
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a",required = false)String a,
+                             @RequestParam(value = "b", required = false)String b,
+                             @RequestParam(value = "action", defaultValue = "empty")String action,
+                             Model model){
+        System.out.println(a + " " + b + " " + action);
+        String result;
+        double a1 = Double.parseDouble(a);
+        double a2 = Double.parseDouble(b);
+        double res = 0;
+
+        switch (action){
+            case "multiplication":
+                res=a1*a2;
+                break;
+            case "addition":
+                res=a1+a2;
+                break;
+            case "subtraction":
+                res=a1-a2;
+                break;
+            case "division":
+                res=Math.round(a1/a2);
+                break;
+        }
+        result =  String.valueOf(res);
+        model.addAttribute("result", result);
+        return "first/calculator";
     }
 }
