ecc4145861e8479e285ab599ae48275f835eed53
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2022-01-22 08:59:56 +0200

    study http param

--- src/main/java/com/aleksdenni/spring/HelloController.java
+++ src/main/java/com/aleksdenni/spring/HelloController.java
@@ -2,14 +2,11 @@ package com.aleksdenni.spring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
 
-    //@GetMapping("/")
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String sayHello(){
         return "hello_world";
     }
